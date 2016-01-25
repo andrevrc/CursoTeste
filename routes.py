4c9b4bb7b9a2aecce2672e15def4ecf01d7b6514
andrevrc <andrvc782@gmail.com>
2016-01-25 17:11:34 -0300

    Uhuu!

--- routes.py
+++ routes.py
@@ -7,7 +7,7 @@ def index():
 
 @app.route('/hello')
 def hello():
-	return 'Hello World, lorem ipsum dolor sit amet... asdasdas.. asdashjdgashgdjasgdjhasgdjhasgdjgds'
+	return 'Hello World, lorem ipsum dolor sit amet... shgdjasgdjhasgdjhasgdjgds... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
 @app.route('/user/<username>')
 def show_user_profile(username):
